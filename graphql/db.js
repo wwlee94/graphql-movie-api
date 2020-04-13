f12fb6cd201f1d38e8659e93d31a8f4331d93a87
wwlee94 <wwlee94@naver.com>
2020-04-14 00:55:08 +0900

    REST API 로 GraphQL 감싸기

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,33 @@
-export let movies = [
-    {
-        id: 1,
-        name: "WOOWON",
-        score: 10
-    },
-    {
-        id: 2,
-        name: "니콜라스",
-        score: 2
-    },
-    {
-        id: 3,
-        name: "노마드",
-        score: 8
-    },
-    {
-        id: 4,
-        name: "이량화",
-        score: 9
-    }
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?limit=50&minimum_rating=9";
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovie = movies.filter(movie => id === movie.id);
-    return filteredMovie[0];
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`)
+    .then(res = res.json())
+    .then(json => json.data.movies);
 }
 
-export const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanMovies.length ){
-        movies = cleanMovies;
-        return true;
-    } else{
-        return false;
-    }
-}
+// export const getById = id => {
+//     const filteredMovie = movies.filter(movie => id === movie.id);
+//     return filteredMovie[0];
+// }
 
-export const addMovie = (name, score) => {
-    const newMovie ={
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+// export const deleteMovie = id => {
+//     const cleanMovies = movies.filter(movie => movie.id !== id);
+//     if(movies.length > cleanMovies.length ){
+//         movies = cleanMovies;
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
+
+// export const addMovie = (name, score) => {
+//     const newMovie ={
+//         id: `${movies.length + 1}`,
+//         name,
+//         score
+//     };
+//     movies.push(newMovie);
+//     return newMovie;
+// }
