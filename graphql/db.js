dcefe8bf358c0e5fd4d32d43eccbc38592031fa0
wwlee94 <wwlee94@naver.com>
2020-04-14 00:38:38 +0900

    Mutation으로 기존 Movie 데이터 삭제하기

--- graphql/db.js
+++ graphql/db.js
@@ -31,9 +31,19 @@ export const getById = id => {
 export const deleteMovie = id => {
     const cleanMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanMovies.length ){
-        movie = cleanMovies;
+        movies = cleanMovies;
         return true;
     } else{
         return false;
     }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie ={
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,15 @@
-import {getMovies, getById} from './db';
+import {getMovies, getById, addMovie, deleteMovie} from './db';
 
 const resolvers = {
     Query: {
         movies: getMovies,
         movie: (_, args) => getById(args.id),
+        // 위와 동일
+        // movie: (_, {id}) => getById(id),
     },
     Mutation: {
-        addMovie: (_, {name, score})
+        addMovie: (_, {name, score}) => addMovie(name, score),
+        deleteMovie: (_, {id}) => deleteMovie(id)
     }
 };
 export default resolvers;
