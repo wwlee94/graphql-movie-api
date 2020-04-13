44948d421197873dfcc5dc8401cc27776e31238b
wwlee94 <wwlee94@naver.com>
2020-04-14 01:18:49 +0900

    REST API 로 GraphQL 감싸기 - 2

--- graphql/db.js
+++ graphql/db.js
@@ -1,11 +1,18 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.mx/api/v2/list_movies.json?limit=50&minimum_rating=9";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-    fetch(`${API_URL}`)
-    .then(res = res.json())
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
+    }
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
     .then(json => json.data.movies);
-}
+} 
 
 // export const getById = id => {
 //     const filteredMovie = movies.filter(movie => id === movie.id);
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,14 +2,14 @@ import {getMovies, getById, addMovie, deleteMovie} from './db';
 
 const resolvers = {
     Query: {
-        movies: getMovies,
-        movie: (_, args) => getById(args.id),
+        movies: (_, {limit, rating}) => getMovies(limit, rating)
+        // movie: (_, args) => getById(args.id),
         // 위와 동일
         // movie: (_, {id}) => getById(id),
     },
-    Mutation: {
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_, {id}) => deleteMovie(id)
-    }
+    // Mutation: {
+    //     addMovie: (_, {name, score}) => addMovie(name, score),
+    //     deleteMovie: (_, {id}) => deleteMovie(id)
+    // }
 };
 export default resolvers;
